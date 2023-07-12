811e0000ed7af956d768af669444d5a0989fcffe
Alexander Stefański <alex@aps-studio.eu>
2023-07-12 08:34:18 +0200

    v1.0.7

--- src/useRefStatic.ts
+++ src/useRefStatic.ts
@@ -1,6 +1,6 @@
-import { useRef } from "react";
+import { useRef, MutableRefObject } from "react";
 
-export function useRefStatic<T>(staticValue: () => T) {
+export function useRefStatic<T>(staticValue: () => T): MutableRefObject<T> {
     const ref = useRef<T>(null as T);
     if (ref.current === null) {
         ref.current = staticValue();
--- src/useStoreState.ts
+++ src/useStoreState.ts
@@ -2,10 +2,10 @@ import { useEffect, useState } from "react";
 import { StoreReadonly } from "@nexcodepl/store";
 
 export function useStoreState<T>(store: StoreReadonly<T>): T {
-    const [state, setState] = useState<T>(() => store.current());
+    const [state, setState] = useState<T>(() => store.current() as T);
 
     useEffect(() => {
-        const storeUnsubscribe = store.subscribe(value => setState(value));
+        const storeUnsubscribe = store.subscribe(value => setState(value as T));
 
         return () => storeUnsubscribe();
     }, []);
